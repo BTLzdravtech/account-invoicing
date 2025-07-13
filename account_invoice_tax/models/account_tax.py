a096bd1b5f33accd87765c282ee9973836bd5fa6
alain <alainbloos@gmail.com>
2025-07-13 19:33:38 -0300

    [REF] progress on TODOs, marked as "DONETODO vk"

--- account_invoice_tax/models/account_tax.py
+++ account_invoice_tax/models/account_tax.py
@@ -14,38 +14,41 @@ class AccountTax(models.Model):
 
     _inherit = "account.tax"
 
-    # TODO vk: lock only for arg
+    # DONETODO vk: lock only for arg
     @api.model
     def _prepare_tax_totals(self, base_lines, currency, tax_lines=None, is_company_currency_requested=False):
-        totals = super()._prepare_tax_totals(base_lines, currency, tax_lines=tax_lines, is_company_currency_requested=is_company_currency_requested)
-        ##recorrer totals y si comple on la condicion y esta en self.env._context.get('tax_total_origin')
-        tax_total_origin = self.env.context.get('tax_total_origin')
-        if tax_total_origin:
-            tax_list_origin = self.env.context.get('tax_list_origin')
-            for subtotals_order in totals['subtotals_order']:
-                for subtotals_index in range(0,len(totals['groups_by_subtotal'][subtotals_order])):
-                    id_fixed = tax_list_origin.filtered(lambda x: \
-                        x.amount_type == 'fixed' and x.type_tax_use == 'purchase' \
-                        and x.tax_group_id.id == totals['groups_by_subtotal'][subtotals_order][subtotals_index]['tax_group_id'])
+        if self.company_id.country_id == self.env.ref('base.ar'):
+            totals = super()._prepare_tax_totals(base_lines, currency, tax_lines=tax_lines, is_company_currency_requested=is_company_currency_requested)
+            ##recorrer totals y si comple on la condicion y esta en self.env._context.get('tax_total_origin')
+            tax_total_origin = self.env.context.get('tax_total_origin')
+            if tax_total_origin:
+                tax_list_origin = self.env.context.get('tax_list_origin')
+                for subtotals_order in totals['subtotals_order']:
+                    for subtotals_index in range(0,len(totals['groups_by_subtotal'][subtotals_order])):
+                        id_fixed = tax_list_origin.filtered(lambda x: \
+                            x.amount_type == 'fixed' and x.type_tax_use == 'purchase' \
+                            and x.tax_group_id.id == totals['groups_by_subtotal'][subtotals_order][subtotals_index]['tax_group_id'])
 
-                    existent_group = [x for x in tax_total_origin['groups_by_subtotal'][subtotals_order] if x['tax_group_id'] == totals['groups_by_subtotal'][subtotals_order][subtotals_index]['tax_group_id']]
+                        existent_group = [x for x in tax_total_origin['groups_by_subtotal'][subtotals_order] if x['tax_group_id'] == totals['groups_by_subtotal'][subtotals_order][subtotals_index]['tax_group_id']]
 
-                    if id_fixed and existent_group:
-                        totals['groups_by_subtotal'][subtotals_order][subtotals_index] =  existent_group[0]
+                        if id_fixed and existent_group:
+                            totals['groups_by_subtotal'][subtotals_order][subtotals_index] =  existent_group[0]
 
 
-            subtotals = []
-            amount_tax = 0
-            for subtotal_title in totals['subtotals_order']:
+                subtotals = []
+                amount_tax = 0
+                for subtotal_title in totals['subtotals_order']:
+                    amount_total = totals['amount_untaxed'] + amount_tax
+                    subtotals.append({
+                        'name': subtotal_title,
+                        'amount': amount_total,
+                        'formatted_amount': formatLang(self.env, amount_total, currency_obj=currency),
+                    })
+                    amount_tax += sum(x['tax_group_amount'] for x in totals['groups_by_subtotal'][subtotal_title])
+
                 amount_total = totals['amount_untaxed'] + amount_tax
-                subtotals.append({
-                    'name': subtotal_title,
-                    'amount': amount_total,
-                    'formatted_amount': formatLang(self.env, amount_total, currency_obj=currency),
-                })
-                amount_tax += sum(x['tax_group_amount'] for x in totals['groups_by_subtotal'][subtotal_title])
 
-            amount_total = totals['amount_untaxed'] + amount_tax
-
-            totals['amount_tax'] = amount_tax
-        return totals
+                totals['amount_tax'] = amount_tax
+            return totals
+        else:
+            return super()._prepare_tax_totals(base_lines, currency, tax_lines=tax_lines, is_company_currency_requested=is_company_currency_requested)
--- website_sale_account_invoice_commission/models/account_commission_rule.py
+++ website_sale_account_invoice_commission/models/account_commission_rule.py
@@ -15,18 +15,19 @@ class AccountCommissionRule(models.Model):
         auto_join=True,
     )
 
-    # TODO vk: lock only for arg
-    def _get_rule_domain(
-            self, date, product, partner_id, customer, amount):
-        domain = super()._get_rule_domain(
-            date, product, partner_id, customer, amount)
-        if not product:
-            domain += [('public_category_id', '=', False)]
+    # DONETODO vk: lock only for arg
+    def _get_rule_domain(self, date, product, partner_id, customer, amount):
+        if self.company_id.country_id == self.env.ref('base.ar'):
+            domain = super()._get_rule_domain(date, product, partner_id, customer, amount)
+            if not product:
+                domain += [('public_category_id', '=', False)]
+            else:
+                domain += [
+                    '|',
+                    ('public_category_id', '=', False),
+                    ('public_category_id', 'parent_of',
+                        product.public_categ_ids.ids),
+                ]
+            return domain
         else:
-            domain += [
-                '|',
-                ('public_category_id', '=', False),
-                ('public_category_id', 'parent_of',
-                    product.public_categ_ids.ids),
-            ]
-        return domain
+            return super()._get_rule_domain(date, product, partner_id, customer, amount)
