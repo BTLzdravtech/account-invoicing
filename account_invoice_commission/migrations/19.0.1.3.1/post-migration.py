48bf521ce98cc2518e820f898ac6c20b274c6886
Ondřej Šírek <sireko@btlnet.com>
2026-09-11 18:29:32 +0200

    fix: remove migration recompute account_move

--- account_invoice_commission/migrations/19.0.1.3.1/post-migration.py
+++ account_invoice_commission/migrations/19.0.1.3.1/post-migration.py
@@ -4,7 +4,8 @@ from odoo import SUPERUSER_ID, api
 def migrate(cr, version):
     env = api.Environment(cr, SUPERUSER_ID, {})
     last_id = 0
-    while moves := env["account.move"].search([("id", ">", last_id)], order="id", limit=10000):
-        env.add_to_compute(moves._fields["date_last_payment"], moves)
-        moves._recompute_recordset(["date_last_payment"])
-        last_id = moves[-1].id
+    # TODO: Upgrade 19.0 - this is crazy change to AR only records
+    # while moves := env["account.move"].search([("id", ">", last_id)], order="id", limit=10000):
+    #     env.add_to_compute(moves._fields["date_last_payment"], moves)
+    #     moves._recompute_recordset(["date_last_payment"])
+    #     last_id = moves[-1].id
